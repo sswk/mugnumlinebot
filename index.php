e22d968de8ab543b8834d1f00cc11e1aea686e1f
sswk <pacific.wk.ho@gmail.com>
2017-09-26 23:18:49 +0900

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ foreach ($events as $event) {
   */
   
   if(strpos($event->getText(),'占') !== false){
-   $my_reply = "マグナムポイント　＋" + (string)mt_rand(0,100) + "!!"
+   $my_reply = "マグナムポイント　＋" . (string)mt_rand(0,100) . "!!"
    $bot->replyText($event->getReplyToken(), $my_reply);
   } else{
    $bot->replyText($event->getReplyToken(), "チャオ♪　マグナムだよ(^_-)-☆"); 
